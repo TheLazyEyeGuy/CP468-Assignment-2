4115c4fb67a470d63d38214e8815620f9ec068e8
DerekMarshall855 <derekmarshall855@gmail.com>
2020-11-09 11:29:13 -0500

    Added check to ensure length of puzzle correct. Will still crash if puzzle contains character that is not numerical from 0-9

--- main.py
+++ main.py
@@ -15,33 +15,36 @@ f.close()
 flatNums = reduce(lambda z, y :z + y, numbers)
 
 # Convert flattened list into a sudoku puzzle
-sudoku = Sudoku(flatNums)
-print("ORIGINAL: ")
-sudoku.print_sud()
-# Run ac3
-if ac3(sudoku):
-    if sudoku.solved():  # Check if each domain length is 1, if so puzzle solved using a3 algorithm alone
-        print("Puzzle solved using AC3, print output: ")
-        sudoku.print_sol()
-    else:
-        assignment = {}
-
-        for i in sudoku.cells:
-            if len(sudoku.poss[i]) == 1:
-                assignment[i] = sudoku.poss[i][0]  # If we found cell value already, assign it
-
-            assignment = back_track(assignment, sudoku)  # Use backtrack alg on ac3 reduced domain space
-
-        for i in sudoku.poss:
-            sudoku.poss[i] = assignment[i] if len(i) > 1 else sudoku.poss[i]
-
-        if assignment:
-            print("Solution has been found using backtracking algorithm: ")
+if len(flatNums) == 81:
+    sudoku = Sudoku(flatNums)
+    print("ORIGINAL: ")
+    sudoku.print_sud()
+    # Run ac3
+    if ac3(sudoku):
+        if sudoku.solved():  # Check if each domain length is 1, if so puzzle solved using a3 algorithm alone
+            print("Puzzle solved using AC3, print output: ")
             sudoku.print_sol()
         else:
-            print("There is no solution for this puzzle")
-else:  # ac3 failed
-    print("The puzzle entered was found to be arc inconsistent with the ac3 algorithm")
+            assignment = {}
+
+            for i in sudoku.cells:
+                if len(sudoku.poss[i]) == 1:
+                    assignment[i] = sudoku.poss[i][0]  # If we found cell value already, assign it
+
+                assignment = back_track(assignment, sudoku)  # Use backtrack alg on ac3 reduced domain space
+
+            for i in sudoku.poss:
+                sudoku.poss[i] = assignment[i] if len(i) > 1 else sudoku.poss[i]
+
+            if assignment:
+                print("Solution has been found using backtracking algorithm: ")
+                sudoku.print_sol()
+            else:
+                print("There is no solution for this puzzle")
+    else:  # ac3 failed
+        print("The puzzle entered was found to be arc inconsistent with the ac3 algorithm")
+else:
+    print("Input from file is invalid, program stopped")
 #sudoku.print_sud()
 
 #sudoku.AC3()
